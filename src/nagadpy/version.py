051eeff5451f67161d0e18034e18881b4b017575
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2023-09-29 02:18:37 +0600

    feat: minor changes

--- src/nagadpy/version.py
+++ src/nagadpy/version.py
@@ -1,6 +1,1 @@
-import os
-
-__version__ = "0.0.3"
-
-if os.environ.get("ENV") == "test":
-    __version__ += "-test"
+__version__ = "0.0.1"
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,38 +1,32 @@
-import os
-import sys
-
-import pytest
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-
+import unittest
 
 from nagadpy.utils import generate_challenge, get_timestamp
 
 
-class TestUtils:
-    def test_generate_challenge_returns_string(self):
+class TestUtils(unittest.TestCase):
+    def test_generate_challenge_returns_string(self) -> None:
         result = generate_challenge(40)
         assert isinstance(result, str), "Generated challenge should be a string"
 
-    def test_generate_challenge_has_correct_length(self):
+    def test_generate_challenge_has_correct_length(self) -> None:
         length = 40
         result = generate_challenge(length)
         assert len(result) == length, f"Generated challenge length should be {length}"
 
-    def test_generate_challenge_contains_only_lowercase_letters(self):
+    def test_generate_challenge_contains_only_lowercase_letters(self) -> None:
         result = generate_challenge(40)
         assert (
             result.islower()
         ), "Generated challenge should contain only lowercase letters"
 
-    def test_get_timestamp_returns_string(self):
+    def test_get_timestamp_returns_string(self) -> None:
         result = get_timestamp()
         assert isinstance(result, str), "Timestamp should be a string"
 
-    def test_get_timestamp_has_correct_length(self):
+    def test_get_timestamp_has_correct_length(self) -> None:
         result = get_timestamp()
         assert len(result) == 14, "Timestamp length should be 14 characters"
 
-    def test_get_timestamp_contains_only_digits(self):
+    def test_get_timestamp_contains_only_digits(self) -> None:
         result = get_timestamp()
         assert result.isdigit(), "Timestamp should contain only digits"
